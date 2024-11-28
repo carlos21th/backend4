f2528603feecf92b161e8cb8739c2688db2b7009
carlos21th <carlosht2003@hotmail.com>
2024-11-28 17:52:59 -0600

    Add files via upload

--- models/note.js
+++ models/note.js
@@ -1,33 +1,29 @@
 const mongoose = require ('mongoose')
-
+ 
 const url= process.env.MONGODB_URI
  
 mongoose.set('strictQuery', false)
-console.log('Conneting to',url);
-
+console.log('Connecting to', url);
+ 
 mongoose.connect(url)
-    .then(result =>{
-        console.log('conected to MongoDB');
+    .then(result => {
+        console.log('Connected to MongoDB');
     })
-    .catch(error =>{
-        console.log('error connecting to MondgDB', error.message);
+    .catch(error => {
+        console.log('error connecting to MongoDB', error.message);
     })
-
  
 const noteSchema = new mongoose.Schema({
 content: String,
 important: Boolean
 })
- 
-noteSchema.set('toJSON,') ,{
-    Transform:(document, returnObject) => {
-        returnObject.id=returnObject.id.toString()
-        delete returnObject._id
-        delete returnObject._v
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id=returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
     }
-
-
-}
-
-
+})
+ 
 module.exports = mongoose.model('Note', noteSchema)
+ 
